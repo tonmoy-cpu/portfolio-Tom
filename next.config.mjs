242c220e2011c0b7d4a39f96bc43d2700d7abd3b
Tonmoy Mukherjee <73432344+tonmoy-cpu@users.noreply.github.com>
2025-01-12 07:20:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      loader: "akamai",
+      path: "/",
+    },
+  };
+  
+  module.exports = nextConfig;
